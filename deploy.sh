aff52bc337d349342907559694b1b4f9d1063425
L. de Kimpe <electrenator2000@protonmail.com>
2023-04-19 21:38:24 +0200

    Moved site sources to `src`

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-scp ./index.html root@users.t-huis.lgbt:/var/www/html/index.html
-scp -r ./static/ root@users.t-huis.lgbt:/var/www/html/static
+scp -r ./src/index.html ./src/static/ root@users.t-huis.lgbt:/var/www/html/
